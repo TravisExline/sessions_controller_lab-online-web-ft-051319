11e763000f1aabdf6313b98064ef09cae01be33a
Travis Exline <t.exline22@gmail.com>
2019-08-01 14:39:57 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if params[:name].present?
+    if !params[:name].present?
       redirect_to sessions_new_path
     else
       session[:name] = params[:name]
